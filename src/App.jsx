f2b121e02280fbbd35803cbe3657dff860d047f9
Michele Brignola <michele.brignola2@gmail.com>
2026-02-15 16:18:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/index.css";
+
+export default function App() {
+  return (
+    <>
+      <h1>Layout</h1>
+      <ul>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+    </>
+  );
+}
