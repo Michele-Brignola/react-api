630049e0fc4fac627baf491e63d47d1d5a63077c
Michele Brignola <michele.brignola2@gmail.com>
2026-02-15 16:26:08 +0100

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/index.css";
 
 export default function App() {
-  return (
-    <>
-      <h1>Layout</h1>
-      <ul>
-        <li>React</li>
-        <li>Bootstrap</li>
-      </ul>
-    </>
-  );
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    const fetchActors = async () => {
+      try {
+        const response = await axios.get(
+          "https://lanciweb.github.io/demo/api/actors/",
+        );
+        console.log("Lista attori:", response.data);
+        setActors(response.data);
+      } catch (error) {
+        console.error("Errore nel caricamento:", error);
+      }
+    };
+    fetchActors();
+  }, []);
+
+  return <></>;
 }
